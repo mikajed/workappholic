401813722d21444b2c6f69210bd1ae280829b9f2
mikajed <michaeljedrz@gmail.com>
2023-07-25 19:31:00 +0200

    add js

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// fetch eingabedaten
+let firma1 = prompt('Firma: ');
+console.log(firma1);
+
+let contactperson1 = prompt('Ansprechpartner: ');
+console.log(contactperson1);
+
+let job1 = prompt('Tätigkeit: ');
+console.log(job1);
+
+let email1 = prompt('E-Mail: ');
+console.log(email1);
+
+let phone1 = prompt('Telefonnummer: ');
+console.log(phone1);
