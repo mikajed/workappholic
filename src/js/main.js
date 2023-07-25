8255bd4302ca72e6789b4ebf908258a18971b0f3
mikajed <michaeljedrz@gmail.com>
2023-07-25 20:11:22 +0200

    add eingabedaten js

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,37 @@
 // fetch eingabedaten
-let firma1 = prompt('Firma: ');
-console.log(firma1);
+let firma1 = prompt('Firma:');
+console.log(`Firma: ${firma1}`);
 
-let contactperson1 = prompt('Ansprechpartner: ');
-console.log(contactperson1);
+let contactperson1 = prompt('Ansprechpartner:');
+console.log(`Ansprechpartner: ${contactperson1}`);
 
-let job1 = prompt('Tätigkeit: ');
-console.log(job1);
+let job1 = prompt('Tätigkeit:');
+console.log(`Tätigkeit: ${job1}`);
 
-let email1 = prompt('E-Mail: ');
-console.log(email1);
+let email1 = prompt('E-Mail:');
+console.log(`E-Mail: ${email1}`);
 
-let phone1 = prompt('Telefonnummer: ');
-console.log(phone1);
+let phone1 = prompt('Telefonnummer:');
+console.log(`Telefonnummer: ${phone1}`);
+
+let datum1 = prompt('Datum (jjjj-mm-tt):');
+console.log(`Datum: ${datum1}`);
+
+
+let firma2 = prompt('Firma:');
+console.log(`Firma: ${firma2}`);
+
+let contactperson2 = prompt('Ansprechpartner:');
+console.log(`Ansprechpartner: ${contactperson2}`);
+
+let job2 = prompt('Tätigkeit:');
+console.log(`Tätigkeit: ${job2}`);
+
+let email2 = prompt('E-Mail:');
+console.log(`E-Mail: ${email2}`);
+
+let phone2 = prompt('Telefonnummer:');
+console.log(`Telefonnummer: ${phone2}`);
+
+let datum2 = prompt('Datum (jjjj-mm-tt):');
+console.log(`Datum: ${datum2}`);
